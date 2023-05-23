4785259637bb0deedb4a631160948d503acf8a72
ChunBaee <mljung10@naver.com>
2023-05-23 14:22:14 +0900

    feat: 스플래시 화면 Ui 임시 추가

---
+++ app/src/main/java/com/c7z/mappilogue_aos/presentation/ui/splash/view/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.c7z.mappilogue_aos.presentation.ui.splash.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.c7z.mappilogue_aos.presentation.ui.main.view.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //:TODO 임시로 1초 정지 후 메인화면 이동 -> 추후 자동로그인 이후 이동으로 변경 예정
+        Thread.sleep(1000)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
